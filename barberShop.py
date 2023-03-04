49c650b94e1da1b58b4c452770eb1173f6c4d553
Samuel Hancak <79135406+SamuelHancak@users.noreply.github.com>
2023-03-04 09:24:12 +0100

    feat: implementation of problem solution

---
+++ barberShop.py
@@ -0,0 +1,165 @@
+"""
+Program represents a solution of the Sleeping Barber Problem.
+
+Sleeping Barber Problem is a scenario where multiple customers arrive at a barbershop to get a haircut,
+but there is only one barber who can serve them.
+The program uses mutex and rendezvous primitives from the FEI-PPDS library to synchronize the
+interactions between the customers and the barber.
+
+University: STU Slovak Technical University in Bratislava
+Faculty: FEI Faculty of Electrical Engineering and Information Technology
+Year: 2023
+"""
+
+__authors__ = "Marián Šebeňa, Samuel Hančák"
+__email__ = "mariansebena@stuba.sk, xhancaks@stuba.sk"
+__license__ = "MIT"
+
+from fei.ppds import Mutex, Thread, Semaphore, print
+from time import sleep
+from random import randint
+
+NUM_OF_CUSTOMERS = 5
+SIZE_OF_WAITING_ROOM = 3
+# randomness of function execution is decided by the `randint()` function in range 1 and `RANDOMNESS` variable
+RANDOMNESS = 5
+
+
+class Shared(object):
+
+    def __init__(self):
+        self.mutex = Mutex()
+        self.waiting_room = 0
+        self.customer = Semaphore(0)
+        self.barber = Semaphore(0)
+        self.customer_done = Semaphore(0)
+        self.barber_done = Semaphore(0)
+
+
+def get_haircut(i):
+    """
+    Simulates time and prints info when customer gets haircut
+
+    @param i: Index of current customer
+    """
+    print(f"CUSTOMER {i}: Gets haircut")
+    sleep(randint(1, RANDOMNESS) / 10)
+
+
+def cut_hair():
+    """
+    Simulates time and prints info when barber cuts customer's hair
+    """
+    print('BARBER Cuts hair')
+    sleep(randint(1, RANDOMNESS) / 10)
+
+
+def balk(i):
+    """
+    Represents situation when waiting room is full and prints info
+
+    @param i: Index of current customer
+    """
+    print(f"CUSTOMER {i}: Walks away 'cause waiting room is full")
+    sleep(randint(1, RANDOMNESS) / 10)
+
+
+def growing_hair(i):
+    """
+    Represents situation when customer waits after getting haircut.
+    So hair is growing and customer is sleeping for some time.
+
+    @param i: Index of current customer
+    """
+    print(f"CUSTOMER {i}: Is growing his hair")
+    sleep(randint(1, RANDOMNESS) / 10)
+
+
+def waiting_in_room(i, shared):
+    """
+    Simulates time and prints info when customer is waiting in the waiting room
+
+    @param i: Index of current customer
+    @param shared: Shared object of needed variables
+    """
+    print(f"CUSTOMER {i}: Is waiting in room - rooms left {SIZE_OF_WAITING_ROOM - shared.waiting_room}")
+    sleep(randint(1, RANDOMNESS) / 10)
+
+
+def customer(i, shared):
+    """
+    Function represents customers behaviour. Customer come to waiting if room is full sleep.
+    Wake up barber and waits for invitation from barber. Then gets new haircut.
+    After it both wait to complete their work. At the end waits to hair grow again.
+
+    @param i: Index of current customer
+    @param shared: Shared object of needed variables
+    """
+
+    while True:
+        # access to waiting room
+        shared.mutex.lock()
+        if shared.waiting_room < SIZE_OF_WAITING_ROOM:
+            shared.waiting_room += 1
+            # print(f"CUSTOMER {i}: Is waiting in room - {shared.waiting_room}")
+            waiting_in_room(i, shared)
+            shared.mutex.unlock()
+        else:
+            shared.mutex.unlock()
+            balk(i)
+            continue
+
+        # rendezvous 1
+        shared.customer.signal()
+        shared.barber.wait()
+
+        get_haircut(i)
+
+        # rendezvous 2
+        shared.customer_done.signal()
+        shared.barber_done.wait()
+
+        # leave waiting room
+        shared.mutex.lock()
+        shared.waiting_room -= 1
+        shared.mutex.unlock()
+
+        # wait for hair to grow again
+        growing_hair(i)
+
+
+def barber(shared):
+    """
+    Function barber represents barber. Barber is sleeping.
+    When customer come to get new hair wakes up barber.
+    Barber cuts customer hair and both wait to complete their work.
+
+    @param shared: Shared object of needed variables
+    """
+
+    while True:
+        # rendezvous 1
+        shared.customer.wait()
+        shared.barber.signal()
+
+        cut_hair()
+
+        # rendezvous 2
+        shared.customer_done.wait()
+        shared.barber_done.signal()
+
+
+def main():
+    shared = Shared()  # object of shared variables
+    customers = []  # array of customers - for each customer is appended Thread to array
+
+    for i in range(NUM_OF_CUSTOMERS):
+        customers.append(Thread(customer, i, shared))
+    hair_stylist = Thread(barber, shared)
+
+    for t in customers + [hair_stylist]:
+        t.join()
+
+
+if __name__ == "__main__":
+    main()
